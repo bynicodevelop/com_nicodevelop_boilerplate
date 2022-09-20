4534c9e52880283f44879228ca911485a345b7b3
nico-develop <contact@nico-develop.fr>
2022-09-20 19:08:55 +0200

    Intégration du theme de base - Fixed #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:json_theme/json_theme.dart';
+
+import 'package:flutter/services.dart'; // For rootBundle
+import 'dart:convert'; // For jsonDecode
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Récupère le fichier de configuration theme
+  final themeStr = await rootBundle.loadString('assets/theme.json');
+
+  /// Conversion du fichier en objet
+  final themeJson = jsonDecode(themeStr);
+
+  /// Conversion en theme Flutter
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  runApp(MyApp(theme: theme));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeData theme;
+
+  const MyApp({
+    Key? key,
+    required this.theme,
+  }) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: theme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
