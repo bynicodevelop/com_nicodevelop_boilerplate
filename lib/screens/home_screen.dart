9d6144d5f0314df592d43ff5dbe039496e3360da
nico-develop <contact@nico-develop.fr>
2022-09-20 19:01:03 +0200

    Intégration de la traduction - Fixed #2

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:com_nicodevelop_dotmessenger/utils/translate.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dot Messenger'),
+      ),
+      body: Center(
+        child: Text(t(context)!.helloWorld),
+      ),
+    );
+  }
+}
