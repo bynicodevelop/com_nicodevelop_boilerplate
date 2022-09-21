acebe37a6fb97412fb283a497ee180444c1ba295
nico-develop <contact@nico-develop.fr>
2022-09-21 06:44:55 +0200

    Mise en place des bases sur la liste des messages

---
+++ lib/components/list_messages/bloc/get_list_message_event.dart
@@ -0,0 +1,23 @@
+part of 'get_list_message_bloc.dart';
+
+abstract class GetListMessageEvent extends Equatable {
+  const GetListMessageEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OnGetListMessageEvent extends GetListMessageEvent {}
+
+class OnMessageLoadedEvent extends GetListMessageEvent {
+  final List<ItemMessageModel> listMessage;
+
+  const OnMessageLoadedEvent({
+    required this.listMessage,
+  });
+
+  @override
+  List<Object> get props => [
+        listMessage,
+      ];
+}
---
+++ lib/components/list_messages/list_messages_component.dart
@@ -0,0 +1,28 @@
+import 'package:com_nicodevelop_dotmessenger/components/item_message/item_message_component.dart';
+import 'package:com_nicodevelop_dotmessenger/components/list_messages/bloc/get_list_message_bloc.dart';
+import 'package:com_nicodevelop_dotmessenger/models/item_message_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ListMessagesComponent extends StatelessWidget {
+  const ListMessagesComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetListMessageBloc, GetListMessageState>(
+      builder: (context, state) {
+        final List<ItemMessageModel> listMessage =
+            (state is GetListMessageInitialState) ? state.listMessage : [];
+
+        return ListView.builder(
+          itemCount: listMessage.length,
+          itemBuilder: (context, index) {
+            return ItemMessageComponent(
+              itemMessage: listMessage[index],
+            );
+          },
+        );
+      },
+    );
+  }
+}
