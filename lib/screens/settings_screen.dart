0547b9ed76fba445a84050e9ad6e05b93bb5ca9d
nico-develop <contact@nico-develop.fr>
2022-09-26 07:18:15 +0200

    Ajout d'un redirection après l'authentification - fixed #57

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import "package:com_nicodevelop_dotmessenger/models/user_model.dart";
+import "package:com_nicodevelop_dotmessenger/screens/authentication/signin_screen.dart";
 import "package:com_nicodevelop_dotmessenger/services/authentication_status/authentication_status_bloc.dart";
 import "package:com_nicodevelop_dotmessenger/services/logout/logout_bloc.dart";
 import "package:com_nicodevelop_dotmessenger/utils/translate.dart";
@@ -13,48 +14,60 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Settings",
+    return BlocListener<LogoutBloc, LogoutState>(
+      listener: (context, state) async {
+        if (state is LogoutSuccessState) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const SignInScreen()),
+            (route) => false,
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Settings",
+          ),
         ),
-      ),
-      body: settingsScreen([
-        {
-          "title": t(context)!.setting_item_profile_title,
-          "subtitle": t(context)!.setting_item_profile_description,
-          "onTap": () async {
-            final UserModel userModel = (context
-                    .read<AuthenticationStatusBloc>()
-                    .state as AuthenticatedStatusState)
-                .userModel;
+        body: settingsScreen([
+          {
+            "title": t(context)!.setting_item_profile_title,
+            "subtitle": t(context)!.setting_item_profile_description,
+            "onTap": () async {
+              final UserModel userModel = (context
+                      .read<AuthenticationStatusBloc>()
+                      .state as AuthenticatedStatusState)
+                  .userModel;
 
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ProfileScreen(
-                  userModel: userModel,
-                ),
-              ),
-            );
-          },
-        },
-        {
-          "title": t(context)!.setting_item_about_title,
-          "subtitle": t(context)!.setting_item_about_description,
-          "onTap": () async => Navigator.push(
+              Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => const AboutScreen(),
+                  builder: (context) => ProfileScreen(
+                    userModel: userModel,
+                  ),
                 ),
-              ),
-        },
-        {
-          "title": t(context)!.setting_item_logout_title,
-          "subtitle": t(context)!.setting_item_logout_description,
-          "onTap": () async => context.read<LogoutBloc>().add(OnLogoutEvent()),
-        }
-      ]),
+              );
+            },
+          },
+          {
+            "title": t(context)!.setting_item_about_title,
+            "subtitle": t(context)!.setting_item_about_description,
+            "onTap": () async => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AboutScreen(),
+                  ),
+                ),
+          },
+          {
+            "title": t(context)!.setting_item_logout_title,
+            "subtitle": t(context)!.setting_item_logout_description,
+            "onTap": () async =>
+                context.read<LogoutBloc>().add(OnLogoutEvent()),
+          }
+        ]),
+      ),
     );
   }
 }
